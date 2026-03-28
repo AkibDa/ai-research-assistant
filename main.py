6d9e591918df453735ac12667913cc5125ddd22f
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2026-03-28 19:19:26 +0530

    now the code is running as intended without any response from the api yet

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-from fastapi import FastAPI
-from app.routes import chat, upload
 import uvicorn
 
-app = FastAPI(title="AI Research Assistant")
-
-app.include_router(chat.router, prefix="/chat")
-app.include_router(upload.router, prefix="/upload")
-
-@app.get("/")
-def root():
-    return {"message": "API is running"}
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True)
